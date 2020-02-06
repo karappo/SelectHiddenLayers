b7891d00e844df3aeedfa0a7da83973708cc433a
Naokazu Terada <naokazu.terada@gmail.com>
2020-02-06 16:31:15 +0900

    非表示判定を再帰的に

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+
 function selectHiddenLayers(selection) {
-  selection.items = selection.items[0].children.filter(function (childNode) {
-    console.log(childNode)
-    return !childNode.visible
-  })
+  selection.items = getHiddenChildren(selection.items[0])
+}
+function getHiddenChildren (node, array = []) {
+  if (!node.visible) {
+    array.push(node)
+  } else if (0 < node.children.length) {
+    node.children.forEach(function (child) {
+      if (!child.visible) {
+        array.push(child)
+      } else {
+        array.concat(getHiddenChildren(child, array))
+      }
+    })
+  }
+  return array
 }
 module.exports = {
   commands: {
