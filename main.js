55b02d1f5130e1478f8af7a507ee21d400b08921
Naokazu Terada <naokazu.terada@gmail.com>
2020-02-06 15:43:21 +0900

    eslint settings

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-function selectHiddenLayers (selection) {
-    selection.items = selection.items[0].children.filter(function (childNode) {
-        return !childNode.visible
-    })
+function selectHiddenLayers(selection) {
+  selection.items = selection.items[0].children.filter(function (childNode) {
+    console.log(childNode)
+    return !childNode.visible
+  })
 }
 module.exports = {
-    commands: {
-        selectHiddenLayers
-    }
+  commands: {
+    selectHiddenLayers
+  }
 }
